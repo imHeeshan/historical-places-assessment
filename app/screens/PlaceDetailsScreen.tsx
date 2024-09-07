d53d2815e16631b5ee3a400f5a9ca53118a46810
imHeeshan <imHeeshan@gmail.com>
2024-09-07 09:43:44 +0530

    install important package and file structure

--- app/screens/PlaceDetailsScreen.tsx
+++ app/screens/PlaceDetailsScreen.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const PlaceDetailsScreen = () => {
   return (
     <View>
-      <Text>Well come to react native</Text>
+      <Text>PlaceDetailsScreen</Text>
     </View>
   )
 }
 
-export default App
+export default PlaceDetailsScreen
---
+++ app/screens/SplaceScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SplaceScreen = () => {
+  return (
+    <View>
+      <Text>SplaceScreen</Text>
+    </View>
+  )
+}
+
+export default SplaceScreen
